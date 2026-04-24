51e18ce44a25976658c5c2405eca5c57bda46627
Mohamed Eslam <your_email@example.com>
2026-04-24 18:15:34 +0300

    Update site

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+// 👇 السطر الأول الجديد: بنعرف الكود على أداة الإحصائيات
+import { Analytics } from '@vercel/analytics/react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    {/* 👇 السطر الثاني الجديد: بنشغل الأداة جوه الموقع */}
+    <Analytics />
   </StrictMode>,
 );
