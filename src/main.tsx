6c761979d6244d0de1e0bdfbaf3e3592eba8fd13
Mohamed Eslam <your_email@example.com>
2026-05-02 00:04:09 +0300

    Update site

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import './i18n'; // 👈 السطر ده اللي كان ناقص عشان الترجمة تشتغل!
+
 // 👇 السطر الأول الجديد: بنعرف الكود على أداة الإحصائيات
 import { Analytics } from '@vercel/analytics/react';
 
