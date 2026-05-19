79d48f0704a4d5829fbbf163dd8b1f5bb997c5b9
Mohamed Eslam <mohamedeslamel99@gmail.com>
2026-05-20 02:15:08 +0300

    Update site

--- src/lib/api/football/[...path].js
+++ src/lib/api/football/[...path].js
@@ -1,22 +1,22 @@
-export default async function handler(req, res) {
-  // بنجيب المسار اللي الرياكت طالبه
-  const { path } = req.query;
-  const pathString = Array.isArray(path) ? path.join('/') : path || '';
+// export default async function handler(req, res) {
+//   // بنجيب المسار اللي الرياكت طالبه
+//   const { path } = req.query;
+//   const pathString = Array.isArray(path) ? path.join('/') : path || '';
   
-  // اللينك الأصلي بتاع موقع الكورة
-  const targetUrl = `https://api.football-data.org/v4/${pathString}`;
+//   // اللينك الأصلي بتاع موقع الكورة
+//   const targetUrl = `https://api.football-data.org/v4/${pathString}`;
 
- try {
-    const response = await fetch(targetUrl, {
-      headers: {
-        // حطينا المفتاح هنا مباشرة للتجربة
-        'X-Auth-Token': '94ad2c35b69c48629558d4f91f65d9e0',
-      },
-    });
+//  try {
+//     const response = await fetch(targetUrl, {
+//       headers: {
+//         // حطينا المفتاح هنا مباشرة للتجربة
+//         'X-Auth-Token': '94ad2c35b69c48629558d4f91f65d9e0',
+//       },
+//     });
 
-    const data = await response.json();
-    res.status(response.status).json(data);
-  } catch (error) {
-    res.status(500).json({ error: 'Failed to fetch data' });
-  }
-}
+//     const data = await response.json();
+//     res.status(response.status).json(data);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Failed to fetch data' });
+//   }
+// }
